6ddc733486d285fd52e35be4db81d5dc72d8ade0
haved <krogstie.havard@gmail.com>
2020-09-10 13:29:29 +0200

    it works!

--- extract.py
+++ extract.py
@@ -2,12 +2,7 @@
 
 from subprocess import call
 
-FILE = "build/lib.linux-x86_64-3.8/queuemodule.cpython-38-x86_64-linux-gnu.so"
-
-#call(["python", "setup.py", "build"])
-#call(["objcopy", "--only-section=.text", "-O", "binary", FILE, "text.o"])
-call(["gcc", "-I/usr/include/python3.8/", "-c", "queuemodule.c", "-o", "queuemodule.o"])
-#call(["gcc", "queuemodule.o", "-o", "a.o"])
+call(["gcc", "-I/home/havard/Development/cpython/Include", "-I/home/havard/Development/cpython", "-c", "queuemodule.c", "-o", "queuemodule.o"])
 call(["objcopy", "--only-section=.text", "-O", "binary", "queuemodule.o", "text.o"])
 with open("text.o", "rb") as fil:
     with open("text.py", "w") as ut:
--- queuemodule.c
+++ queuemodule.c
@@ -2,55 +2,40 @@
 #include <Python.h>
 #include <stdio.h>
 
+#define front data[-3]
+#define back data[-2]
+#define max_size data[-1]
+#define array ((PyObject**)data)
+
 #pragma GCC optimize ("align-functions=16")
 
-void data_func();
-
-#define front data[0]
-#define back data[1]
-#define max_size data[2]
-#define array ((PyObject**)data+4)
+#define PyCFunction_GET_FUNCTION(func)              \
+  (((PyCFunctionObject *)func) -> m_ml -> ml_meth)
 
 int setCallAddress(PyObject *obj, void *address) {
-  int offset = Py_TYPE(obj)->tp_vectorcall_offset;
-  vectorcallfunc *func = (vectorcallfunc *)(((char *)obj) + offset);
-  *func = address;
-  return offset;
+  PyCFunction_GET_FUNCTION(obj) = address;
+  return 0;
 }
 
-//args is a normal C array of object pointers
-//nargsf is the amount, with a flag bit set sometimes
-PyObject *make_superqu(PyObject *self, PyObject *const *args, size_t nargsf, PyObject *kwargs) {
-  int offset = Py_TYPE(self)->tp_vectorcall_offset;
-  vectorcallfunc func = *(vectorcallfunc *)(((char *)self) + offset);
-  size_t *data = ((char*)func) + 0x400-0x60;
-
-  //max_size is set using memmove
+int setMaxSize(int mx, PyObject* self, size_t* data) {
   front = 0;
   back = 0;
-
-  Py_INCREF(self);
-  return self;
+  max_size = mx;
+  *((size_t**)(self)+0x10) = data;
+  return 0;
 }
 
-PyObject *superenqu(PyObject *self, PyObject *const *args, size_t nargsf, PyObject *kwargs) {
-  int offset = Py_TYPE(self)->tp_vectorcall_offset;
-  vectorcallfunc func = *(vectorcallfunc *)(((char *)self) + offset);
-  size_t *data = ((char*)func) + 0x400-0xe0;
-
-  PyObject *item = args[0];
+PyObject *superenqu(PyObject *self, PyObject* item) {
+  size_t *data = *((size_t**)(self)+0x10);
 
   Py_INCREF(item);
   array[back++ % max_size] = item;
 
-  Py_INCREF(self);
-  return self;
+  Py_INCREF(item);
+  return item;
 }
 
-PyObject *superdequ(PyObject *self, PyObject *const *args, size_t nargsf, PyObject *kwargs) {
-  int offset = Py_TYPE(self)->tp_vectorcall_offset;
-  vectorcallfunc func = *(vectorcallfunc *)(((char *)self) + offset);
-  size_t *data = ((char*)func) + 0x400-0x1a0;
-
+PyObject *superdequ(PyObject *self, PyObject* item) {
+  size_t *data = *((size_t**)(self)+0x10);
   return array[front++ % max_size];
 }
